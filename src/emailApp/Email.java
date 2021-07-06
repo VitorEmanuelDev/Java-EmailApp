4f743f0a5529fe75e4c9fe251cfad52bf75b28c6
vitoremanueldev <veslima3@gmail.com>
2021-07-06 02:30:17 -0300

    Setting up a menu

--- src/emailApp/Email.java
+++ src/emailApp/Email.java
@@ -1,5 +1,7 @@
 package emailApp;
 
+import java.util.Scanner;
+
 public class Email {
 	private String firstName;
 	private String lastName;
@@ -8,21 +10,43 @@ public class Email {
 	private int mailBoxCapacity;
 	private String alternateEmail;
 	
+	//Constructor to receive the first and last names
 	public Email(String firstName, String lastName) {
 		
 		this.firstName = firstName;
 		this.lastName = lastName;
 		System.out.println("Email created: " + this.firstName + " " + this.lastName);
 		
+		//Call a method asking for the department and return a department
+		this.department = setDepartment();
+		System.out.println("Department: " + this.department);
 		
 	}
 	
-	
+	//Ask for the department
 	private String setDepartment() {
 		
-		System.out.print("Enter the department\n1 Sales\n2 Development\n3 Accounting\n0 None");
+		System.out.print("Pick a department\n1 Sales\n2 Development\n3 Accounting\n0 None");
 		Scanner in = new Scanner(System.in);
-		int 
+		int departmentChoice = in.nextInt();
+		
+		if(departmentChoice == 1) {
+			
+			return "sales";
+	
+		}else if (departmentChoice == 2) {
+		
+			return "dev";
+		
+		}else if (departmentChoice == 3) {
+		
+			return "acct";
+		
+		}else {
+		
+			return "";
+		
+		}
 		
 	}
 
